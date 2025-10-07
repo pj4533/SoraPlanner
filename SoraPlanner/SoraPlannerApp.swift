e997395ee93477b5d1396c1c1ec144c72447536c
PJ Gray <pj4533@gmail.com>
2025-10-07 16:41:18 -0400

    Initial Commit

---
+++ SoraPlanner/SoraPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoraPlannerApp.swift
+//  SoraPlanner
+//
+//  Created by PJ Gray on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SoraPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoraPlannerTests/SoraPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoraPlannerTests.swift
+//  SoraPlannerTests
+//
+//  Created by PJ Gray on 10/7/25.
+//
+
+import Testing
+@testable import SoraPlanner
+
+struct SoraPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
